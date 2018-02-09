1750f690dbf2414bf7462fd7ec4b24a2aa11aba5
Frank Wang <dorankme@gmail.com>
2018-02-09 08:52:43 +0800

    Site updated: 2018-02-09 08:52:42

---
+++ js/src/bootstrap.js
@@ -0,0 +1,3 @@
+// build time:Fri Feb 09 2018 08:52:41 GMT+0800 (中国标准时间)
+$(document).ready(function(){Even.backToTop();Even.mobileNavbar();Even.toc();Even.fancybox();Even.visits()});
+//rebuild by neat 
